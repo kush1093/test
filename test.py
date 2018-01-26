39d376ffd1f98843ebdddd57f6723225448b57ca
Kushal Rajashekhar Mestri <mestri@bld-ml-00035306.olympus.f5net.com>
2018-01-26 13:12:46 -0700

    first repo-nth commit

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-print ("hi there")
-if(True):
-	print("Yayyyyyyyyyyyyy")
-else:
-	print ("haha")
-	print("Noooooooooo")
-kushal
+import os
+import sys
+
+def func():
+	return "Hi there! this is my first repository on GIT-HUB"
+
+if(__name__ == "__main__"):
+	print ("Testing testing")
+	a = func()
+	print (a)
