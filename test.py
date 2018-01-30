d28da96f00d9a1569492e3ccf1043f8c80348a5b
kush1093 <k.mestri@f5.com>
2018-01-30 10:18:47 -0700

    11

--- test.py
+++ test.py
@@ -1,16 +1,26 @@
 import os
 import sys
+import os
 
 def func():
-	return "Hi there! this is my first repository on GIT-HUB"
+	#return "Hi there! this is my first repository on GIT-HUB"
+    try:
+        #a=pow(2,100000000000)
+        a=1/0 
+    except ArithmeticError:
+        print("yay")
+    except ZeroDivisionError:
+        print("yo yo honey singh")
+    except Exception as e:
+        print(e)
 
 if(__name__ == "__main__"):
-	print ("Testing testing")
-	a = func()
-	print (a)
-    print ("from t2-folder")
+    #print ("Testing testing")
+    func()
+    #print (a)
+    #print ("from t2-folder")
 
-    print("In Boulder")
-    print("with new SSH keys")
+    #print("In Boulder")
+    #print("with new SSH keys")
 
 
